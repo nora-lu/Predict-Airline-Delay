3527e162526483a03fe55a11627cd063ad3ac344
chemsslu <chem.sslv@gmail.com>
2016-03-30 20:04:30 -0400

    Added comment on how to install MySQLdb package

--- read_data.py
+++ read_data.py
@@ -1,3 +1,5 @@
+#MySQLdb is installed as http://www.tutorialspoint.com/python/python_database_access.htm
+#conda command may also work but not tested heres
 import MySQLdb
 
 config = {'host':"cs586.cpzwisqfd2pn.us-west-2.rds.amazonaws.com",
