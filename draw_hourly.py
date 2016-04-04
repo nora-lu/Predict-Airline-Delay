ebf561a270b2856455c75e44207952fca98d3606
Qian Lu <nora-lu@users.noreply.github.com>
2016-04-03 23:05:48 -0400

    Added files via upload

---
+++ draw_hourly.py
@@ -0,0 +1,25 @@
+from process import processdata
+import pandas as pd
+from matplotlib import pyplot as plt
+
+files = ['ontime_2010_final.csv', 'ontime_2011_final.csv', 'ontime_2012_final.csv',
+          'ontime_2013_final.csv', 'ontime_2014_final.csv', 'ontime_2015_final.csv']
+
+delay_percentage_by_hour = pd.DataFrame()
+for file in files:
+    print(file)
+    df = processdata(file)
+
+    ########### Yearly line chart of delay percentage by hours #########
+    df['hour'] = df['crs_dep_time'].map(lambda x: int(str(int(x)).zfill(4)[:2]))
+    grouped_total = df[['id','hour']].groupby('hour').count()
+    grouped_delay = df[df.arr_del15 == 1][['arr_del15', 'hour']].groupby('hour').count()
+    print("Total flights", grouped_total)
+    print("Delay flights", grouped_delay)
+
+    delay_percentage_by_hour[file[7:11]] = grouped_delay.arr_del15/grouped_total.id
+
+    # release memory space
+    del df
+
+delay_percentage_by_hour.plot(kind='line')
